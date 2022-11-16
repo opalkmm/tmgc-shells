526e7b321c42eefa3d1daee7240883357c5d515a
opalkmm <ornrumpa.km@gmail.com>
2022-11-16 09:53:06 -0800

    shellnames href prety-stuffed jsonObject

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const fs = require("fs");
+const rp = require("request-promise");
+//TO-DO for getting all images link to be stored in the objects
+//get href from all links
+//let path = "www.tamashell.com" + ${href}
+//getResponsePages = await axios.get(path)
+//const responsePages = response.data
 
 const fetchTitles = async () => {
   try {
@@ -11,21 +17,15 @@ const fetchTitles = async () => {
     const html = response.data;
     const $ = cheerio.load(html);
 
-    /*TODO
-1. get photos corresponding to each model
-*/
-
-    // $("#menu img").each(function () {
-    //   console.log($(this).attr("alt"));
-    // });
-
     //initialize an object to store the models in each category
     const jsonObject = {
-      Vintage: [],
-      Connection: [],
-      Modern: [],
-      Other: []
+      Vintage: {},
+      Connection: {},
+      Modern: {},
+      Other: {}
     };
+
+    //const images = [];
 
     let eachCategory = 0;
 
@@ -34,16 +34,45 @@ const fetchTitles = async () => {
       .each((_idx, el) => {
         if (!$(el).hasClass("pic")) {
           const models = $(el).children("a");
+          let modelName = $(el).text();
+          let holdingArray = [];
 
-          models.each((_idx, el) => {
-            jsonObject[Object.keys(jsonObject)[eachCategory]].push(
-              $(el).text()
-            );
+          models.each(async (_idx, el) => {
+            //make a call to the sub pages and get shell names and their image src links
+            let eachModel = $(el).attr("href");
+            let url = "http://www.tamashell.com/" + eachModel;
+            //images.push($(el).attr("href"));
+            const response = await axios.get(url);
+            // Get the HTML code of the webpage
+            const html = response.data;
+            const $$ = cheerio.load(html);
+
+            //scrape the image links for the sub model pages
+            $$("#content")
+              .find("a")
+              .each((_idx, el) => {
+                if ($$(el).attr("href")) {
+                  //get text and image link
+                  let href = $$(el).attr("href");
+                  let shellName = $$(el).text();
+                  holdingArray.push({
+                    shellName: shellName,
+                    href: href
+                  });
+                }
+              });
+            let thing = jsonObject[Object.keys(jsonObject)[eachCategory]];
+
+            thing[modelName] = holdingArray;
+            console.log(thing);
+            jsonObject = thihng;
           });
         } else {
           eachCategory++;
         }
       });
+    // console.log(jsonObject);
+
     let data = JSON.stringify(jsonObject);
     fs.writeFileSync("tamagotchi-models.json", data);
   } catch (error) {
@@ -53,3 +82,52 @@ const fetchTitles = async () => {
 
 // Print all models in the console
 fetchTitles().then((titles) => console.log(titles));
+
+/*
+const scrapeImages = function (callback) {
+  console.log("Scraping the images succeeded");
+  const baseUrl = "http://www.tamashell.com/";
+  //get all href from those
+  const options = {
+    uri: baseUrl + "/vehicles.html",
+    transform: function (body) {
+      return cheerio.load(body);
+    }
+  };
+  rp(options)
+    .then(($) => {
+      $(".wsite-image.wsite-image-border-none").each(function (i, elem) {
+        var vehicleText = $(elem).find("div").text();
+        var vehicleDescription = vehicleText
+          .split("$")[0]
+          .replace("SOLD", "")
+          .replace(/\r?\n/g, "")
+          .trim();
+        var vehiclePrice = vehicleText.split("$")[1];
+        var vehicle = {
+          site: "AB Autos and Imports",
+          image: baseUrl + $(elem).find("img").attr("src"),
+          url: baseUrl + $(elem).find("a").attr("href"),
+          description: vehicleDescription,
+          shortDescription: "",
+          price: vehiclePrice
+            ? "$" + vehiclePrice.replace(/\r?\n/g, "").replace(".", "")
+            : "",
+          isAvailable: ""
+        };
+        if (vehicle.price !== "") {
+          vehicles.push(vehicle);
+        }
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    .then(() => {
+      callback(vehicles);
+    });
+};
+
+module.exports = scrapeABAutosAndImports;
+
+*/
---
+++ src/index.js
@@ -0,0 +1,19 @@
+// const express = require("express");
+// const cheerio = require("cheerio");
+// const download = require("node-image-downloader");
+// const request = require("request");
+
+// const app = express();
+// const port = 3000;
+
+// app.get("/", (req, res) => {
+//   let url = "http://www.tamashell.com/p1.php";
+
+//   request(url, (error, response, html) => {
+//     if (!error) {
+//       console.log(html);
+//     }
+//   });
+// });
+
+// app.listen(port);
