03fc8ac5476aafc9a6ff8a7c7a234aa114ca8708
opalkmm <ornrumpa.km@gmail.com>
2022-12-01 20:42:39 -0800

    updated href to be full link. TODO add another scrape layer for licenced nanos

--- server.js
+++ server.js
@@ -1,171 +1,125 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const fs = require("fs");
+const { exit } = require("process");
 const rp = require("request-promise");
-//TO-DO for getting all images link to be stored in the objects
-//get href from all links
-//let path = "www.tamashell.com" + ${href}
-//getResponsePages = await axios.get(path)
-//const responsePages = response.data
+
+function getModels(jsonObject) {
+  return axios.get("http://www.tamashell.com/").then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    let category = "Vintage";
+    let skip = true;
+    $("#middlemenu")
+      .find("p")
+      .each((_idx, el) => {
+        if (!$(el).hasClass("pic")) {
+          $(el)
+            .children("a")
+            .each((_idx, el) => {
+              jsonObject[`${category}`].push({
+                model: $(el).text(),
+                href: $(el).attr("href"),
+              });
+            });
+
+          skip = false;
+        } else {
+          if (skip === false) {
+            category = $(el).children("img").attr("alt");
+            //   console.log("new cat: " + category);
+          }
+        }
+        // console.log(jsonObject);
+        return jsonObject;
+      });
+    return jsonObject;
+  });
+}
+async function getShellVersions(jsonObject, allModels) {
+  let holdingArray = [];
+  let object = {};
+  return axios
+    .get("http://www.tamashell.com/" + jsonObject.href)
+    .then((response) => {
+      // and do this:
+
+      const html = response.data;
+      const $$ = cheerio.load(html);
+
+      $$("#content")
+        .find("a")
+        .each((_idx, el) => {
+          if ($$(el).attr("href")) {
+            //get text and image link
+            let href = $$(el).attr("href");
+            let shellName = $$(el).text();
+            holdingArray.push({
+              shellName: shellName,
+              href: "http://www.tamashell.com/" + href,
+            });
+          }
+        });
+
+      object[`${jsonObject.model}`] = holdingArray;
+    })
+    .then(() => {
+      return object;
+    });
+}
+async function getShellDetails(jsonObject) {
+  //incoming array for each model in the category: Vintage [P1, P2, etc.]
+  // we have an array of objects
+
+  for (let i = 0; i < jsonObject.length; i++) {
+    let allModels = [];
+
+    // need to get the call and iterated it before knowing we can move on
+    let allShellVersions = await getShellVersions(jsonObject[i], allModels);
+
+    jsonObject[i] = allShellVersions;
+  }
+
+  return jsonObject;
+}
 
 const fetchTitles = async () => {
   try {
-    // Go to the tamagotchi shell library
-    const response = await axios.get("http://www.tamashell.com/");
-
-    // Get the HTML code of the webpage
-    const html = response.data;
-    const $ = cheerio.load(html);
-
-    //initialize an object to store the models in each category
     let jsonObject = {
       Vintage: [],
       Connection: [],
       Modern: [],
-      Other: []
+      Others: [],
     };
 
-    //const images = [];
+    let blankJsonObject = {
+      Vintage: [],
+      Connection: [],
+      Modern: [],
+      Others: [],
+    };
+    let finalJsonObject = {
+      Vintage: [],
+      Connection: [],
+      Modern: [],
+      Others: [],
+    };
+    let jsonWithModels = await getModels(jsonObject);
 
-    let eachCategory = 0;
+    for (const [key, value] of Object.entries(finalJsonObject)) {
+      let shellDetails = await getShellDetails(jsonWithModels[`${key}`]);
 
-    $("#middlemenu")
-      .find("p")
-      .each((_idx, el) => {
-        let currentCat = "Vintage";
-        console.log("IS IT: ", $(el).hasClass("pic"));
-        if (!$(el).hasClass("pic")) {
-          // const myPromise = new Promise((resolve, reject) => {
-          //   resolve;
-          // });
-
-          // myPromise.then(console.log("promise .then"));
-
-          const models = $(el).children("a");
-
-          let holdingArray = [];
-
-          models.each(async (modelindex, el) => {
-            //make a call to the sub pages and get shell names and their image src links
-            let modelName = $(el).text();
-            let eachModel = $(el).attr("href");
-            let url = "http://www.tamashell.com/" + eachModel;
-            //images.push($(el).attr("href"));
-            const response = await axios.get(url);
-            // Get the HTML code of the webpage
-            const html = await response.data;
-            const $$ = cheerio.load(await html);
-
-            //scrape the image links for the sub model pages
-            $$("#content")
-              .find("a")
-              .each((_idx, el) => {
-                if ($$(el).attr("href")) {
-                  //get text and image link
-                  let href = $$(el).attr("href");
-                  let shellName = $$(el).text();
-                  holdingArray.push({
-                    shellName: shellName,
-                    href: href
-                  });
-                }
-              });
-
-            //vintage, modern, etc
-            let category = jsonObject[Object.keys(jsonObject)[eachCategory]];
-            // this a pointer that points within the jsonObject
-
-            // console.log(category);
-            // console.log(category[modelName]);
-            //vintage{p1: []}
-            if (
-              category !== undefined &&
-              !!category[`${modelName}`] !== undefined
-            ) {
-              category[`${modelName}`] = holdingArray;
-
-              console.log("jsonObject", typeof jsonObject, jsonObject);
-
-              // return jsonObject;
-            }
-            // return jsonObject;
-            if (modelindex === models.length - 1) {
-              console.log("last one here");
-
-              eachCategory++;
-              // at the very end this is where we actually write the data
-            }
-          });
-        } else {
-          currentCat = $(el).find("img").attr("alt");
-          console.log("changing cat to : ", currentCat);
-          // eachCategory++;
-          console.log("has pic class skip");
-        }
-      });
-    // console.log(jsonObject);
-    // console.log(typeof jsonObject, jsonObject);
-
-    let data = JSON.stringify(jsonObject);
+      finalJsonObject[`${key}`] = shellDetails;
+    }
+    let data = JSON.stringify(finalJsonObject);
     fs.writeFileSync("src/tamagotchi-models.json", data);
+    return finalJsonObject;
+    // Get the HTML code of the webpage
   } catch (error) {
     throw error;
   }
 };
 
 // Print all models in the console
-fetchTitles().then((titles) => console.log(titles));
+fetchTitles().then((titles) => console.log("DONE"));
 // fetchTitles();
-
-/*
-const scrapeImages = function (callback) {
-  console.log("Scraping the images succeeded");
-  const baseUrl = "http://www.tamashell.com/";
-  //get all href from those
-  const options = {
-    uri: baseUrl + "/vehicles.html",
-    transform: function (body) {
-      return cheerio.load(body);
-    }
-  };
-  rp(options)
-    .then(($) => {
-      $(".wsite-image.wsite-image-border-none").each(function (i, elem) {
-        var vehicleText = $(elem).find("div").text();
-        var vehicleDescription = vehicleText
-          .split("$")[0]
-          .replace("SOLD", "")
-          .replace(/\r?\n/g, "")
-          .trim();
-        var vehiclePrice = vehicleText.split("$")[1];
-        var vehicle = {
-          site: "AB Autos and Imports",
-          image: baseUrl + $(elem).find("img").attr("src"),
-          url: baseUrl + $(elem).find("a").attr("href"),
-          description: vehicleDescription,
-          shortDescription: "",
-          price: vehiclePrice
-            ? "$" + vehiclePrice.replace(/\r?\n/g, "").replace(".", "")
-            : "",
-          isAvailable: ""
-        };
-        if (vehicle.price !== "") {
-          vehicles.push(vehicle);
-        }
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-    .then(() => {
-      callback(vehicles);
-    });
-};
-
-module.exports = scrapeABAutosAndImports;
-
-*/
-
-// PROBLEMS:
-// 1. it's not fully returning the jsonObject
-// 2. the incrementation of eachCategory needs to be AFTER each p chunk that isn't pic clas
